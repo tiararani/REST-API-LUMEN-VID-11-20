501075d7dc4a8d244f93262c438ccbb4a999ddb4
tiararani <87311636+tiararani@users.noreply.github.com>
2022-02-05 16:31:57 +0700

    Add files via upload

---
+++ app/Http/Controllers/PelangganController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pelanggan;
+use Illuminate\Http\Request;
+
+class PelangganController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Pelanggan::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $this->validate($request,[
+            'pelanggan' => 'required',
+            'alamat' => 'required',
+            'telp' => 'required | numeric'
+        ]);
+
+        $pelanggan = Pelanggan::create($request->all());
+
+        return response()->json($pelanggan);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = Pelanggan::where('idpelanggan',$id)->get();
+
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pelanggan $pelanggan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+
+        Pelanggan::where('idpelanggan', $id)->update($request->all());
+
+        return response()->json("data sudah diupdate");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pelanggan  $pelanggan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+
+        Pelanggan::where('idpelanggan',$id)->delete();
+
+        return response()->json("Data sudah dihapus");
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->post('api/register' , ['uses' => 'LoginController@register'] );
+
+$router->post('api/login' , ['uses' => 'LoginController@login'] );
+
+
+$router->group( ['prefix' => 'api','middleware'=>'auth'] , function() use ($router) {
+    $router->get('kategori' , ['uses' => 'KategoriController@index'] );
+
+    $router->get('kategori/{id}' , ['uses' => 'KategoriController@show'] );
+
+    $router->delete('kategori/{id}' , ['uses' => 'KategoriController@destroy'] );
+
+    $router->put('kategori/{id}' , ['uses' => 'KategoriController@update'] );
+
+    $router->post('kategori' , ['uses' => 'KategoriController@create'] );
+
+    $router->get('pelanggan' , ['uses' => 'PelangganController@index'] );
+
+    $router->get('pelanggan/{id}' , ['uses' => 'PelangganController@show'] );
+
+    $router->post('pelanggan' , ['uses' => 'PelangganController@create'] );
+
+    $router->delete('pelanggan/{id}' , ['uses' => 'PelangganController@destroy'] );
+
+    $router->put('pelanggan/{id}' , ['uses' => 'PelangganController@update'] );
+
+    $router->post('menu' , ['uses' => 'MenuController@create'] );
+
+    $router->get('menu' , ['uses' => 'MenuController@index'] );
+    
+
+
+
+});
+
